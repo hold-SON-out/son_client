c0e570ad354da6574a9528e95e36432d566a2780
조민수 <chominsu10@gmail.com>
2021-07-13 09:49:21 +0900

    [ADD] useSelect, useSwitch

---
+++ src/Hooks/useSelect.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useSelect = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  return { value, onChange };
+};
+
+export default useSelect;
