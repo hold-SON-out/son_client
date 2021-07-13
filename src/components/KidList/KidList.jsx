69987dbad914992243dfc4955db86e3f696149cd
조민수 <chominsu10@gmail.com>
2021-07-13 14:57:05 +0900

    [ADD] kidList

--- src/components/KidList/KidList.jsx
+++ src/components/KidList/KidList.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import KidCard from "../KidCard/KidCard";
+import "./KidList.css";
 
-const KidList = () => {
+const KidList = ({ kids }) => {
   return (
     <div className="kidlist">
       <div className="content">
         <div className="k-title">kid</div>
+        <div className="k-list">
+          {kids.map(kid => (
+            <KidCard kid={kid} />
+          ))}
+        </div>
       </div>
     </div>
   );
